212b4f2a8fea00d4af905ce82ce528d7dfcb4ba4
nicogol09 <nicolaaass345@gmail.com>
2024-08-02 22:14:49 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('contact-form');
 
+    form.addEventListener('submit', (event) => {
+        event.preventDefault(); // Evita o envio real do formulário
+        alert('Obrigado por entrar em contato! Sua mensagem foi recebida.');
+        form.reset(); // Limpa os campos do formulário
+    });
+});
